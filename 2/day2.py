02dd245e9613f78fd68191d8511111c4e125523d
Sam Bentley <sam.bentley@withriley.com>
2023-12-03 17:20:26 +1100

    day 2 - part 1 - solved

--- 2/day2.py
+++ 2/day2.py
@@ -4,33 +4,41 @@ import re
 with open('input.txt') as f:
     input = [line.rstrip('\n') for line in f]
 
+max_cubes = {
+    "red" : 12,
+    "green": 13,
+    "blue" : 14
+}
+
 def split_line(line):
     line = line.split()
     del line[0:2]
     return line
 
 def create_dict_from_input(line):
-    game = {}
+    game = {
+    "red" : 0,
+    "green": 0,
+    "blue" : 0
+    }
     line = split_line(line)
     for i in range(0, len(line), 2):
         colour = line[i + 1]
         colour = re.sub("[^a-zA-Z]", "", colour)
-        number = line [i]
-        if colour not in games:
-            games[colour] = number
-        if games[colour] < number:
-            games[colour] = number
+        number = int(line[i])
+        if int(game[colour]) < int(number):
+            game[colour] = number
     return game
-
-
 
 games = {}
 
 for i in range(len(input)):
-    game = create_dict_from_input(input[i])
-    print(game)
+    games[i + 1] = create_dict_from_input(input[i])
 
-# print(games)
+sum = 0
 
-# games = create_dict_from_input(input)
-# print(games)
+for game,cubes in games.items():
+    if (max_cubes["blue"] >= cubes["blue"]) and (max_cubes["red"] >= cubes["red"]) and (max_cubes["green"] >= cubes["green"]):
+        sum += game
+        
+print(sum)
