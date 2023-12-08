f5a202745288c95f4c0e7c3ee6eea956eacf7618
Sam Bentley <sam.bentley@withriley.com>
2023-12-08 18:27:32 +1100

    wip - maybe works? 🙃

--- day_8/day8.py
+++ day_8/day8.py
@@ -23,49 +23,41 @@ for line in input[2:]:
     map[line[0]] = [line[1], line[2]]
 
 ### PART 1 ###
-# process number of moves
-# num_moves = 0
-# location = "AAA"
-# lost =  True
+num_moves = 0
+location = "AAA"
+lost =  True
 
-# while lost:
-#     for i in range(len(moves)):
-#         num_moves += 1
-#         location = map[location][index[moves[i]]]
-#         if location == "ZZZ":
-#             lost = False
+while lost:
+    for i in range(len(moves)):
+        num_moves += 1
+        location = map[location][index[moves[i]]]
+        if location == "ZZZ":
+            lost = False
 
-# print(f"Part 1 - Number of Moves: {num_moves}")
+print(f"Part 1 - Number of Moves: {num_moves}")
 
 ### PART 2 ###
 num_moves = 0
-# pos_1 = "11A"
-# pos_2 = "22A"
 lost =  True
 positions = []
 
-print(map)
-
+# find start positions
 for key in map:
     print(key)
     if key.endswith("A"):
         positions.append(key)
 
-print(positions)
-
+# find the way home via BRUTE FORCE! 
 while lost:
     for move in range(len(moves)): # iterate over moves. ie. L or R
         for position in range(len(positions)): # iterate over each position (ie. make multiple moves simaltaneously)
-            # print(move) # 0
-            # print(position) # AAA
             positions[position] = map[positions[position]][index[moves[move]]]
         num_moves += 1
         correct_positions = 0
-        for position in positions:
+        for position in positions: # see how many positions are correct
             if position.endswith("Z"):
                 correct_positions += 1
-        print(positions)
-        if correct_positions == len(positions):
+        if correct_positions == len(positions): # determine if we have all correct positions!
             lost = False
 
 print(f"Part 2 - Number of Moves: {num_moves}")
