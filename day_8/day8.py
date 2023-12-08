497430c1ebfd62839421c0e41b791af95627325e
Sam Bentley <sam.bentley@withriley.com>
2023-12-08 17:32:37 +1100

    wip - 🍝

--- day_8/day8.py
+++ day_8/day8.py
@@ -1,11 +1,6 @@
 import re
 
-# input = """LLR
-
-# AAA = (BBB, BBB)
-# BBB = (AAA, ZZZ)
-# ZZZ = (ZZZ, ZZZ)"""
-
+# input = """"""
 # input = input.splitlines()
 
 from aocd import get_data
@@ -17,7 +12,7 @@ index = {
     "R" : 1
 }
 
-# moves are the first 
+# moves are the first line
 moves = input[0]
 
 # convert input to map
@@ -27,23 +22,51 @@ for line in input[2:]:
     line = line.split()
     map[line[0]] = [line[1], line[2]]
 
+### PART 1 ###
 # process number of moves
+# num_moves = 0
+# location = "AAA"
+# lost =  True
+
+# while lost:
+#     for i in range(len(moves)):
+#         num_moves += 1
+#         location = map[location][index[moves[i]]]
+#         if location == "ZZZ":
+#             lost = False
+
+# print(f"Part 1 - Number of Moves: {num_moves}")
+
+### PART 2 ###
 num_moves = 0
-location = "AAA"
+# pos_1 = "11A"
+# pos_2 = "22A"
 lost =  True
+positions = []
+
+print(map)
+
+for key in map:
+    print(key)
+    if key.endswith("A"):
+        positions.append(key)
+
+print(positions)
 
 while lost:
-    for i in range(len(moves)):
+    for move in range(len(moves)): # iterate over moves. ie. L or R
+        for position in range(len(positions)): # iterate over each position (ie. make multiple moves simaltaneously)
+            # print(move) # 0
+            # print(position) # AAA
+            positions[position] = map[positions[position]][index[moves[move]]]
         num_moves += 1
-        moving_to = map[location][index[moves[i]]]
-        location = moving_to
-        if location == "ZZZ":
+        correct_positions = 0
+        for position in positions:
+            if position.endswith("Z"):
+                correct_positions += 1
+        print(positions)
+        if correct_positions == len(positions):
             lost = False
 
-print(f"Part 1 - Number of Moves: {num_moves}")
-
-
-
-
-
+print(f"Part 2 - Number of Moves: {num_moves}")
 
