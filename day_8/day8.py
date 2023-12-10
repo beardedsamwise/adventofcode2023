da79e344b707a16257ab55dd5ca79f27747eafa5
Sam Bentley <sam.bentley@withriley.com>
2023-12-10 17:27:26 +1100

    solved! 🤌

--- day_8/day8.py
+++ day_8/day8.py
@@ -1,5 +1,9 @@
 import re
+import numpy as np
+import time
 from aocd import get_data
+
+start = time.time()
 
 puzzle = get_data(day=8, year=2023).splitlines()
 
@@ -34,28 +38,37 @@ while lost:
 print(f"Part 1 - Number of Moves: {num_moves}")
 
 ### PART 2 ###
-num_moves = 0
-lost =  True
+
 positions = []
+position_totals = []
 
 # find start positions
 for key in puzzle_map:
     if key.endswith("A"):
         positions.append(key)
 
-# find the way home via BRUTE FORCE! 
-# This sadly does not seem to work (at least not in a reasonable time) - Reddit indicates I need to be using LCM
-while lost:
-    for move in range(len(moves)): # iterate over moves. ie. L or R
-        positions = list(map( lambda p: puzzle_map[p][index[moves[move]]], positions ))
-        num_moves += 1
-        correct_positions = 0
-        for position in positions: # see how many positions are correct
-            if position.endswith("Z"):
-                correct_positions += 1
-                print(f"FOUND A Z! - {positions}")
-                input()
-    
-        if correct_positions == len(positions): # determine if we have all correct positions!
-            lost = False
+# Originally tried the way home via BRUTE FORCE! Reddit tells me this would take 182 days and that LCM is the way 
+# This code is mine, but I needed the Reddit tip to find the shortest path to victory
 
+for position in positions:
+    num_moves = 0
+    lost =  True
+    while lost:
+        for i in range(len(moves)):
+            num_moves += 1
+            position = puzzle_map[position][index[moves[i]]]
+            if position[-1] == "Z":
+                position_totals.append(num_moves)
+                lost = False
+
+# find lowest common multiple for the time is takes each position to reach "Z" which is indicative of the number of moves
+# it takes for ALL positions to reach "Z"
+arr = np.array(position_totals)
+x = np.lcm.reduce(arr)
+
+end = time.time()
+
+print(f"Part 2 - Number of Moves: {x}")
+
+print("The time of execution of above program is :",
+      (end-start) * 10**3, "ms")
